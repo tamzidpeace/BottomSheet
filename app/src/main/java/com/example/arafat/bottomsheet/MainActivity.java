5929d8869b3048ef254e83f38be00ab49360904f
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-02-10 20:32:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/arafat/bottomsheet/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.arafat.bottomsheet;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomSheetBehavior bottomSheetBehavior;
+    private TextView mTextViewState;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        mTextViewState = findViewById(R.id.text_view_state);
+
+        Button buttonExpand = findViewById(R.id.button_expand);
+        Button buttonCollapse = findViewById(R.id.button_collapse);
+
+        buttonExpand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+        buttonCollapse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+                switch (i) {
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        mTextViewState.setText("Collapsed");
+                        break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        mTextViewState.setText("Dragging");
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        mTextViewState.setText("Expanded");
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        mTextViewState.setText("Setting");
+                        break;
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        mTextViewState.setText("Hidden");
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+
+            }
+        });
+    }
+}
